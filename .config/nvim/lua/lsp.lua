9272ee8790207da7649d07136d026ea591043f45
aarthvr-b <assefarthur@gmail.com>
2025-11-03 18:04:24 +0100

    Temporanealy removed functions from lsp.

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -1,34 +1,3 @@
-
-vim.api.nvim_create_autocmd("LspAttach", {
-    group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-    callback = function(ev)
-        -- Buffer local mappings. 
-        -- See `:help vim.lsp.*` for docs
-
-
-        local map = vim.keymap.set
-
-        -- set keybinds
-        map('n', "gR", "<cmd>Telescope lsp_references<CR>", { desc = 'Show LSP references'})
-        map('n', "gD", "vim.lsp.buf.declaration", { desc = 'show declarations'})
-        map('n', "gd", "vim.lsp.buf.definition", { desc = 'Show lsp definition'})
-        map({'n','v'}, "<leader>ca", vim.lsp.buf.code_action, { desc = "See available code actions, in `v` mode it will apply to selection"})
-
-        map.set("n", "[d", function()
-            vim.diagnostic.jump({ count = -1, float = true })
-        end, { desc  = "Go to previous diagnostic"} ) -- jump to previous diagnostic in buffer
-
-        map.set("n", "]d", function()
-            vim.diagnostic.jump({ count = 1, float = true })
-        end, {desc = "Go to next diagnostic"}) -- jump to next diagnostic in buffer
-
-        map.set("n", "K", vim.lsp.buf.hover, {desc = "Show documentation for what is under cursor" }) -- show documentation for what is under cursor
-
-        map.set("n", "<leader>rs", ":LspRestart<CR>", {desc = "Restart LSP"} ) -- mapping to restart lsp if necessary
-    end,
-}
-)
-
 local severity = vim.diagnostic.severity
 
 vim.diagnostic.config({
