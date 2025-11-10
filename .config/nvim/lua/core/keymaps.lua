dd47a47e7de568238d9c4e26ce1fbaffdf81ae54
aarthvr-b <assefarthur@gmail.com>
2025-11-10 14:54:24 +0100

    adjusting small typos and linting

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -7,27 +7,26 @@ vks("n", "<leader>bd", ":bd!<CR>", { desc = "Delete current Buffer" })
 vks("n", "<leader>ww", ":write<CR>", { desc = "Write document" })
 vks("n", "<leader>qq", ":q!<CR>", { desc = "Quit without saving" })
 vks("n", "<leader>bn", ":bnext <CR>", { desc = "Tab goes to next Buffer" })
-vks("n", "<leader>bp", ":bprevious <CR>", { desc = "Tab goes to next Buffer" })
-vks("n", "<leader>pv", ":Ex<CR>", { desc = "Toggle netrw tree view" })
+vks("n", "<leader>bp", ":bprevious <CR>", { desc = "Go to previous Buffer" })
+vks("n", "<leader>pv", ":Ex<CR>", { desc = "Open file tree." })
 
-vks('n', "<C-u>", "<C-u>zz", { desc = 'move up and center cursor'})
-vks('n', "<C-d>", "<C-d>zz", { desc = 'move down and center cursor'})
+vks("n", "<C-u>", "<C-u>zz", { desc = "move up and center cursor" })
+vks("n", "<C-d>", "<C-d>zz", { desc = "move down and center cursor" })
 
 -- Togglers
-vks('n', '<leader>wr', ":set wrap! <CR>", { desc = 'activates word wrap setting'})
-vks('n', '<leader>h', ":nohlsearch<CR>", { desc = "Clear search highlights"})
+vks("n", "<leader>wr", ":set wrap! <CR>", { desc = "activates word wrap setting" })
+vks("n", "<Esc>", ":nohlsearch<CR>", { desc = "Clear search highlights" })
 
 -- Visual Keymaps
-vks("v","<leader>rh", "\"hy:%s/<C-r>h//g<left><left>") -- In Visual mode find and replace selected section
-vks("v","J", ":m '>+1<CR>gv=gv") -- Move selected row down
-vks("v","K", ":m '>-2<CR>gv=gv") -- Move selected row up
+vks("v", "<leader>rh", '"hy:%s/<C-r>h//g<left><left>', { desc = "Find and replace selected text" }) -- In Visual mode find and replace selected section
+vks("v", "J", ":m '>+1<CR>gv=gv") -- Move selected row down
+vks("v", "K", ":m '>-2<CR>gv=gv") -- Move selected row up
 
 -- easier interaction with the system Clipboard
-vks({'n','x'}, '<leader>yy', '"+y', { desc = 'copy to system Clipboard'})
-vks({'n','x'}, '<leader>pp', '"+p', { desc = 'Paste from system Clipboard after cursor'})
-vks({'n','x'}, '<leader>PP', '"+P', { desc = 'Paste from system Clipboard before cursor'})
+vks({ "n", "x" }, "<leader>yy", '"+y', { desc = "copy to system Clipboard" })
+vks({ "n", "x" }, "<leader>pp", '"+p', { desc = "Paste from system Clipboard after cursor" })
+vks({ "n", "x" }, "<leader>PP", '"+P', { desc = "Paste from system Clipboard before cursor" })
 
 -- indenting
-vks('v', '<', '<gv', { silent = true, noremap = true, desc = 'Indent selected to the left'})
-vks('v', '>', '>gv', { silent = true, noremap = true, desc = 'Indent selection to the right'})
-
+vks("v", "<", "<gv", { silent = true, noremap = true, desc = "Indent selected to the left" })
+vks("v", ">", ">gv", { silent = true, noremap = true, desc = "Indent selection to the right" })
