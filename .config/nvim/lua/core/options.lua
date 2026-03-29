158385b692dd7bc4bbe2aa0a2809b162a69c19ba
aarthvr-b <assefarthur@gmail.com>
2026-03-29 23:26:09 +0200

    tweaked some small options

--- .config/nvim/lua/core/options.lua
+++ .config/nvim/lua/core/options.lua
@@ -19,13 +19,26 @@ o.expandtab = true
 o.autoindent = true
 
 -- Visual settings
+o.ignorecase = true
+o.smartcase = true
 o.termguicolors = true
--- o.signcolumn = "yes"
--- o.colorcolumn = "80"
+o.signcolumn = "yes"
+o.colorcolumn = "120"
 o.winborder = "rounded"
+o.showmatch = true
 
 -- Behavior settings
 o.selection = "exclusive"
 o.mouse = "a"
 o.clipboard:append("unnamedplus")
 o.spelllang = "en"
+o.backspace = "indent,eol,start"
+o.iskeyword:append("-")
+o.updatetime = 300
+
+local undodir = vim.fn.expand("~/.vim/undodir")
+if
+	vim.fn.isdirectory(undodir) == 0 -- create if not existant
+then
+	vim.fn.mkdir(undodir, "p")
+end
