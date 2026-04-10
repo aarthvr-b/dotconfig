e78dec3b1878fab9fba60c96170331013a9818e6
aarthvr-b <assefarthur@gmail.com>
2026-04-10 12:25:52 +0200

    clean up conflicting wezterm tab bar config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -24,7 +24,6 @@ config.window_decorations = "RESIZE"
 config.window_background_opacity = 0.8
 config.macos_window_background_blur = 30
 
-config.hide_tab_bar_if_only_one_tab = true
 config.window_padding = {
     left = .5,
     right = .5,
