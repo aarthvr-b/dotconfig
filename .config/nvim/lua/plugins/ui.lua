17c8ba621763781645d15049a730a79d6dd25f80
aarthvr-b <assefarthur@gmail.com>
2026-04-17 20:23:51 +0200

    added bufferline and nvim-colorizer to enhance UI

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -1,6 +1,33 @@
 vim.pack.add({
 	{ src = "https://github.com/nvim-tree/nvim-web-devicons" },
 	{ src = "https://github.com/nvim-lualine/lualine.nvim" },
+	{ src = "https://github.com/catgoose/nvim-colorizer.lua" },
+	{ src = "https://github.com/akinsho/bufferline.nvim" },
+})
+
+require("bufferline").setup({
+	options = {
+		numbers = "buffer_id",
+		always_show_bufferline = true,
+		separator_style = "slant",
+	},
+})
+
+if vim.fn.has("nvim-0.11") == 1 then
+	vim.tbl_flatten = function(t)
+		return vim.iter(t):flatten(math.huge):totable()
+	end
+end
+
+require("colorizer").setup(nil, {
+	css = true,
+	mode = "background",
+	names = false,
+	RGB = true,
+	RRGGBB = true,
+	RRGGBBAA = true,
+	rgb_fn = true,
+	hsl_fn = true,
 })
 
 require("lualine").setup({
